c939c6311ca57f389cb9e1c321d8d59736f48ae7
rdkfx <r.alonso2390@gmail.com>
2022-07-06 11:03:50 +0200

    primeros pasos

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const app = express();
+const port=3000;
+
+app.get('/hello', (req, res) => {
+  res.send('Hi! Ruben ');
+});
+
+app.listen(port, () => console.log('Server ready'));
