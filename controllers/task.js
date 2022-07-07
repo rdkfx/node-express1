4c7c325d1ce841c255782542bdb4704cbf0d063f
rdkfx <r.alonso2390@gmail.com>
2022-07-07 14:37:26 +0200

    crud task

--- controllers/task.js
+++ controllers/task.js
@@ -28,10 +28,10 @@ const Task = require('../models/task');
 
 }
 
-async function getTask(req,res){
+async function getTasks(req,res){
 
     try{
-        const tasks= await Task.find().sort({create_at: -1});
+        const tasks= await Task.find({completed: false}).sort({create_at: -1});
 
         if(!tasks)
         {
@@ -46,7 +46,80 @@ async function getTask(req,res){
 
     }
 }
+
+async function getTask(req,res){
+
+    const id_task = req.params.id
+    console.log(`llamando a getTask ${id_task}`);
+    
+    try{
+        const task = await Task.findById(id_task);
+        if(!task)
+        {
+            res.status(400).send("Id no encontrado");
+        }
+        else{
+            res.status(200).send(task);
+        }
+
+
+    }catch(error){
+        res.status(500).send(error);
+    }
+
+}
+
+async function updateTask(req,res){
+
+    const id_task = req.params.id;
+    const params = req.body;
+    console.log(id_task);
+    console.log(params);
+
+    try{
+        const task = await Task.findByIdAndUpdate(id_task, params);
+        if(!task){
+            res.status(400).send({msg: "no se ha podido actualizar lar tarea"});
+        }
+        else{
+            res.status(200).send("Actualizacion completada");
+        }
+
+    }catch(error){
+        res.status(500).send(error)
+    }
+
+
+
+}
+
+async function deleteTask(req,res){
+
+    const id_task = req.params.id;
+    
+
+    try{
+        const task = await Task.findByIdAndDelete(id_task)
+        
+        if(!task){
+            res.status(400).send({msg: "no se ha podido borrar lar tarea"});
+        }
+        else{
+            res.status(200).send("borrado completada");
+        }
+
+    }catch(error){
+        res.status(500).send(error)
+    }
+
+
+
+}
 module.exports ={
 
-    createTask,getTask,
+    createTask,
+    getTasks,
+    getTask,
+    updateTask,
+    deleteTask,
 }
--- routes/task.js
+++ routes/task.js
@@ -4,6 +4,8 @@ const taskController = require("../controllers/task");
 const api = express.Router()
 
 api.post("/task",taskController.createTask);
-api.get("/task",taskController.getTask);
-
+api.get("/task",taskController.getTasks);
+api.get("/task/:id",taskController.getTask);
+api.put("/task/:id",taskController.updateTask);
+api.delete("/task/:id",taskController.deleteTask);
 module.exports = api;
