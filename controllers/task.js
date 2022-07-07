c5265f4e00bb527f17e1bc890206c41643f39757
rdkfx <r.alonso2390@gmail.com>
2022-07-07 13:48:27 +0200

    a

--- controllers/task.js
+++ controllers/task.js
@@ -28,7 +28,25 @@ const Task = require('../models/task');
 
 }
 
+async function getTask(req,res){
+
+    try{
+        const tasks= await Task.find().sort({create_at: -1});
+
+        if(!tasks)
+        {
+            res.status(400).send({msg: "error de tarea "});
+        }
+        else{
+            res.status(200).send({tasks});
+        }
+
+    }catch(error){
+        res.status(500).send(error);
+
+    }
+}
 module.exports ={
 
-    createTask,
+    createTask,getTask,
 }
--- routes/task.js
+++ routes/task.js
@@ -4,5 +4,6 @@ const taskController = require("../controllers/task");
 const api = express.Router()
 
 api.post("/task",taskController.createTask);
+api.get("/task",taskController.getTask);
 
 module.exports = api;
