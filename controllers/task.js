ecf4a91b39b3aa6ce0249d427f2f95bd258bf102
rdkfx <r.alonso2390@gmail.com>
2022-07-07 13:36:44 +0200

    a

--- controllers/task.js
+++ controllers/task.js
@@ -1,9 +1,31 @@
 const Task = require('../models/task');
 
-function createTask(req,res) {
+ async function createTask(req,res) {
 
-console.log("creando tarea");
-console.log(req.body);
+    const task = new Task();
+    const params=req.body;
+
+    task.title=params.title;
+    task.description=params.description;
+
+    try{
+        const task_store= await task.save();
+
+        if(!task_store)
+        {
+            res.status(400).send({msg: "tarea no guardada"});
+        }
+        else{
+            res.status(200).send({task: task_store});
+        }
+
+    }catch(error){
+        res.status(500).send(error);
+
+    }
+
+
+
 }
 
 module.exports ={
